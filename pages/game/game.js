99db54ca326bf37e10c5ac57e3240520ad358944
hzypzy <1109138356@qq.com>
2018-06-25 19:53:18 +0800

    asdasd

--- pages/game/game.js
+++ pages/game/game.js
@@ -5,8 +5,13 @@ const util = require('../../utils/util.js')
 Page({
   data: {
     lock: false,
-    // 遮罩层唤醒
+    k:0,
+    // 游戏结束 遮罩层唤醒
     showGift: false,
+    
+    // 这个是canvas变量
+    st:0,
+
     key:{},
     logs: [],
     // 倒计时 时间
@@ -24,9 +29,13 @@ Page({
     isclick: false,
     isclick_2: false,
     // 用于储存当前用户点击的event.currentTarget.dataset.option_id值
-    selected:''
+    selected: '',
+    //这个是绘制canvas的定时器
+    time: ''
   },
   onLoad: function () {
+    // wx.clearStorageSync('key')
+    // console.log('onload加载完毕' + this.data.k++)
     // 获取缓存
     var key=wx.getStorageSync('key')
     //获取来自首页缓存的玩家金币数量
@@ -36,9 +45,12 @@ Page({
     
     // 将缓存数据绑定到data
     this.setData({ 
+      // 财产 关数 缓存'key'对象体 勾叉图png隐藏
       property:p,
       ji:j,
-      key:key
+      key:key,
+      hide: true,
+      lock:false
     })
 
     // 选项_数组 不能超过三项
@@ -60,14 +72,15 @@ Page({
       hz_options:options_content
     })
 
+      
       //  倒计时canvas圆形进度条绘制
-      // var step = parseFloat(this.data.step);
+      var step = parseFloat(this.data.step);
       var inte = parseFloat(this.data.interval);
-      var step = Math.PI * 60 / inte;
-      var st=0;
+      var step = Math.PI * 20 / inte;
+      var st=this.data.st;
       var that=this;
 
-      var time = setInterval(function () {
+      this.data.time =setInterval(function () {
 
         // 外圈
         var cxt_arc = wx.createCanvasContext('canvasArc');//创建并返回绘图上下文context对象。  
@@ -95,8 +108,6 @@ Page({
         cxt_arc.stroke();//对当前路径进行描边  
 
 
-
-
         cxt_arc.setLineWidth(6);
         cxt_arc.setStrokeStyle('white');
         cxt_arc.setLineCap('round')
@@ -113,26 +124,57 @@ Page({
           var pro = parseFloat(that.data.property);
           pro -= 10;
           that.setData({
-            lock: true,
+            // 函数截留 (时间到 无法答题)
+            lock: false,
+            // 设置财产变量
             property: pro
           })
-          // 重新设置缓存
+          // 更新缓存
           that.data.key.money_num = pro;
           wx.setStorageSync('key', that.data.key)
-          // clearInterval(time)
+          // 重新赋值canvas变量
           st=0;
         }
 
-      }, 30)
+      }, 10)
+      
+
+
+      // 游戏失败
+      if (this.data.property <= 0) {
+        // 重新设置缓存
+        // this.data.key.money_num = pro;
+        // this.data.key.number_gk = guanqia;
+        // wx.setStorageSync('key', this.data.key)
+
+        // 触发定时器 延时
+        var that = this;
+        setTimeout(function () {
+          that.setData({
+            showGift: true
+          })
+        }, 1000)
+
+      }
 
   },
   // 事件处理函数
 
-  // 玩家答题业务
+
+  // 游戏
+  // 玩家答题
   choose: function (event) {
+    var that=this;
+    //玩家做出选择后 清除定时器 倒计时更新重启 (且无法选择其他项?)
+    clearInterval(this.data.time)
+    this.setData({
+      time:''
+    })
+
+    // 节流防止二次点击
     if(this.data.lock){
       return
-    }else{
+    } else {
 
     // 获取玩家财产 property
     var pro = parseFloat(this.data.property);
@@ -140,25 +182,39 @@ Page({
     var guanqia = parseFloat(this.data.ji);
     // 获取用户选择下标ID
     var id= event.currentTarget.dataset.option_id;
+    // 勾叉图片加载
     this.setData({
       selected: id,
-      hide:false
+      hide: false,
+      lock:true
     })
     //假设后台传来的id为0;
+
     if (id === 0) {
-      pro+=10;
+        // 答案正确
+
+      pro+=100;
       guanqia++;
       this.setData({
-        // 答案正确
         judge: true,
         property: pro,
         ji: guanqia,
       })
+      // 重新设置缓存
+      this.data.key.money_num = pro;
+      this.data.key.number_gk = guanqia;
+      wx.setStorageSync('key', this.data.key)
+
+      // 刷新页面更新选项和题目
+      setTimeout(function () {
+        that.onLoad()
+      },2000)
 
     } else {
-      pro -= 10;
-      this.setData({
         // 答案错误
+
+      pro -= 200;
+      this.setData({
         judge: false,
         property: pro,
       })
@@ -166,38 +222,47 @@ Page({
       this.data.key.money_num = pro;
       this.data.key.number_gk = guanqia;
       wx.setStorageSync('key', this.data.key)
-      // 触发定时器 延时
-      var that=this;
+
+      //玩家做出选择后 清除定时器 倒计时更新重启 (且无法选择其他项?)
+      clearInterval(this.data.time)
+
+      // 刷新页面更新选项和题目
       setTimeout(function () {
+        that.onLoad()
+      }, 2000)
 
-        that.setData({
-        showGift: true
-        })
-        // 回到首页
-        // wx.navigateBack({
-
-        // })
-      },1000)
     };
     }
   },
 
-  // 游戏失败
-  // 关注有好礼遮罩层 唤醒
-  // gift_show: function () {
-  //   this.setData({
-  //     showGift: true
-  //   })
-  // },
   // 关注有好礼遮罩层 隐藏
   gift_hide: function () {
     this.setData({
       showGift: false
     })
+    clearInterval(this.data.time)
         // 回到首页
         wx.navigateBack({
 
         })
+  },
+
+  // 分享得金币
+  get_golds:function(){
+    this.setData({
+      property:2000
+    })
+    // 重新设置缓存
+    this.data.key.money_num = this.data.property;
+    // this.data.key.number_gk = guanqia;
+    wx.setStorageSync('key', this.data.key)
+  },
+
+  //音乐播放  没得用??????????????????????????????????????????????????????????????????????????????
+  music_play: function () {
+    wx.playVoice({
+      filePath: 'http://sc1.111ttt.com/2016/1/09/28/202280605509.mp3',
+    })
   },
 
   onShareAppMessage: function () {
